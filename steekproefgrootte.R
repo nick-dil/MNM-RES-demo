e5ae269bd6c20553afbc924e1acc980fa64da8be
Nick Dillen <47636003+nick-dil@users.noreply.github.com>
2025-11-28 08:07:29 +0100

    move code to functions and add demo cleanup

--- steekproefgrootte.R
+++ steekproefgrootte.R
@@ -1,34 +1,72 @@
 library(googledrive)
-library(googlesheets4)
 
-# inspect input data
-poc_dir_link = "https://drive.google.com/drive/folders/1gzrB-5AG-KYHmiQUThyTEhpsboMPHXeT"
+getPocIdList = function(
+    poc_gdrive_link = "https://drive.google.com/drive/folders/1gzrB-5AG-KYHmiQUThyTEhpsboMPHXeT"){
+  
+  # Authenticate and search for data
+  poc_ls = drive_ls(poc_gdrive_link)
+  
+  # create easier to use (interactively) poc-folder to id mapping in list
+  poc_id_list = as.list(poc_ls$id)
+  names(poc_id_list) = poc_ls$name
+  
+  return(poc_id_list)
+  
+}
 
-poc_ls = drive_ls(poc_dir_link)
-poc_ls
+readPocSampleData = function(poc_drive_id, demo = TRUE){
+  
+  # Get matching CSV file from children
+  my_poc_ls = drive_ls(poc_drive_id, recursive = T,
+                       pattern = "spatial_samples", type = "csv")
+  my_poc_ls
+  
+  # Warning if more than 1 CSV found -> take first
+  # Stop if no CSV found
+  if ( nrow(my_poc_ls) > 1 ){
+    my_poc_ls = my_poc_ls[1,]
+    warning("More than 1 spatial_samples CSV found in folder: ", poc_drive_id, "\nUsing first found: ", my_poc_ls$id)
+  } else if (nrow(my_poc_ls) < 1){
+    stop("No spatial_samples CSV found in folder: ", poc_drive_id)
+  }
+  
+  # Read file from GDrive ID
+  mycsv = read.table(
+    text = drive_read_string(
+      as_id(my_poc_ls)
+    ),
+    header = T,
+    sep = ",",
+    fill = T
+  )
+  
+  if (demo){
+    
+    # Load dependency
+    require(n2khab)
+    hydr_class_lookup = n2khab::read_types()[, c("type", "hydr_class")]
+    
+    # Select hardcoded colnames and translate
+    mycsv = mycsv[, c("scheme", "stratum", "grts_address")]
+    colnames(mycsv) = c('meetnet', "type", "locatie")
+    
+    # Use lookup-table to match stratum (type) with HC
+    mycsv = merge(mycsv, hydr_class_lookup)
+  }
+  
+  return(mycsv)
+}
 
-# create easy to use (interactively) poc-folder to id mapping in list
-poc_id_list = as.list(poc_ls$id)
-names(poc_id_list) = poc_ls$name
 
-# Select poc version from list
-my_poc = "poc_0.10.0"
+#### MAIN ####
+
+# Retrieve POC dir ids ~ version
+poc_id_list = getPocIdList()
+
+# Select poc version from list, e.g. 10
 my_poc_id = poc_id_list$poc_0.10.0
 
-# Get matching CSV file from children
-my_poc_ls = drive_ls(my_poc_id, recursive = T, pattern="spatial_samples", type="csv")
-my_poc_ls
+# Read spatial_sample.csv for corresponding POC from GDrive
+my_poc_data = readPocSampleData(my_poc_id)
 
-# assert nrow == 1
-# .....
-
-# Read file from GDrive ID
-mycsv = read.table(
-  text = drive_read_string(
-    as_id(my_poc_ls)
-    ),
-  header = T,
-  sep = ","
-  )
-
-
+head(my_poc_data)
